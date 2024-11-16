c1ce52158840ea17ff356b4a7fb494a8c1ee4a6f
Sumit240803 <goyalsumit651@gmail.com>
2024-11-16 16:40:41 +0530

    Some more functions

--- travelguide_backend/src/main/java/com/social/travelguide/models/LocalPlaces.java
+++ travelguide_backend/src/main/java/com/social/travelguide/models/LocalPlaces.java
@@ -2,9 +2,12 @@ package com.social.travelguide.models;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class LocalPlaces {
     private String name;
     private Parameters parameters;
-
+    private List<String> about= new ArrayList<>();
 }
--- travelguide_backend/src/main/java/com/social/travelguide/services/TravelService.java
+++ travelguide_backend/src/main/java/com/social/travelguide/services/TravelService.java
@@ -1,12 +1,14 @@
 package com.social.travelguide.services;
 
 import com.social.travelguide.dto.Response;
+import com.social.travelguide.models.LocalPlaces;
 import com.social.travelguide.models.TravelPlaces;
 import com.social.travelguide.repository.TravelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TravelService {
@@ -65,4 +67,45 @@ public class TravelService {
             return response;
         }
     }
+
+    public Response addAbout(String id, String name, String about) {
+        Response response = new Response();
+
+        try {
+            Optional<TravelPlaces> travelPlacesOptional = travelRepository.findById(id);
+
+            if (travelPlacesOptional.isPresent()) {
+                TravelPlaces travelPlace = travelPlacesOptional.get();
+                List<LocalPlaces> localPlaces = travelPlace.getLocalPlaces();
+
+                boolean placeFound = false;
+
+                for (LocalPlaces local : localPlaces) {
+                    if (local.getName().equals(name)) {
+                        local.setAbout(List.of(about)); // Update the `about` field
+                        placeFound = true;
+                        break;
+                    }
+                }
+
+                if (placeFound) {
+                    travelRepository.save(travelPlace); // Save the updated object to the database
+
+                    response.setMessage("About information updated successfully.");
+                } else {
+                    ;
+                    response.setMessage("Local place with the given name not found.");
+                }
+            } else {
+
+                response.setMessage("Travel place with the given ID not found.");
+            }
+        } catch (Exception e) {
+
+            response.setMessage("An error occurred: " + e.getMessage());
+        }
+
+        return response;
+    }
+
 }
